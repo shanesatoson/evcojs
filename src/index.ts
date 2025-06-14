79b2a23ddff12d2e57eaa94c26bbe9be4ce38645
Shane Satoson <shane.satoson@gmail.com>
2025-06-14 16:25:13 +0200

    add jsdoc

--- src/index.ts
+++ src/index.ts
@@ -25,28 +25,47 @@ export interface CloudEvent<T> {
   data: T;
 }
 
+/**
+ * Represents a command handler in a CQRS architecture.
+ *
+ * @template C - The type of data associated with the command.
+ * @type the unique command type
+ * @subjects the subjects to fetch state from
+ */
 interface CommandHandler {
   handle: (command: any, state?: any) => CloudEvent<any>[];
   context: string;
   type: string;
 }
 
+/**
+ * Represents a state rebuilder in a CQRS architecture.
+ */
 interface StateRebuilder {
   stateRebuilder: (event: any, state?: any) => any;
   context: string;
   type: string;
 }
 
+/**
+ * Represents an event handler in a CQRS architecture.
+ */
 interface EventHandler {
   eventHandler: (events: CloudEvent<any>, state?: any) => void;
   type: string;
 }
 
+/**
+ * Represents an upcaster in a CQRS architecture.
+ */
 interface UpCasthandler {
   upcast: (event: CloudEvent<any>) => CloudEvent<any>;
   type: string;
 }
 
+/**
+ * Represents a state loader function in a CQRS architecture.
+ */
 interface StateLoaderFunction {
   load: (subjects: string[]) => Promise<CloudEvent<any>[]>;
   context: string;
@@ -83,6 +102,17 @@ export function registerUpcaster(
   upcastHandler.set(type, { type, upcast: func });
 }
 
+/**
+ * Registers an event handler for a specific event type.
+ *
+ * @param {string} type - The type of the event to register the handler for.
+ * @param {function(CloudEvent<any>, any): void} func - The event handler function
+ * that processes the event and optionally the current state.
+ *
+ * The event handler is added to a list of handlers associated with the event type.
+ * If no handlers exist for the given type, a new list is created.
+ */
+
 export function registerEventhandler(
   type: string,
   func: (event: CloudEvent<any>, state?: any) => void
@@ -93,6 +123,16 @@ export function registerEventhandler(
   eventHandler.get(type)!.push({ type, eventHandler: func });
 }
 
+/**
+ * Registers a function that loads historical events for a specific context.
+ *
+ * The function is called with an array of subjects (e.g., '/book/123') and
+ * should return a Promise that resolves with an array of historical events
+ * associated with the given subjects.
+ *
+ * @param context - The context for which the events are loaded.
+ * @param load - The function that loads the events.
+ */
 export function registerStateLoadingFunction(
   context: string,
   load: (subjects: string[]) => Promise<CloudEvent<any>[]>
@@ -100,6 +140,17 @@ export function registerStateLoadingFunction(
   stateLoader.set(context, { load, context: context });
 }
 
+/**
+ * Registers a command handler for a specific command type.
+ *
+ * The command handler is a function that validates business rules and
+ * returns new events to be saved persistently.
+ *
+ * @param {string} type - The type of the command to register the handler for.
+ * @param {string} context - The context in which the command is executed.
+ * @param {function(any, any): CloudEvent<any>[]} commandHandler - The command handler function.
+ * The function receives the command and the current state and returns an array of new events.
+ */
 export function registerCommandHandler(
   type: string,
   context: string,
@@ -110,6 +161,22 @@ export function registerCommandHandler(
   }
   commands.set(type, { type, context, handle: commandHandler });
 }
+
+/**
+ * Registers a state rebuilder function for a specific event type within a given context.
+ *
+ * The state rebuilder function is responsible for applying the event to the current state
+ * to produce an updated state. It is an essential part of the event sourcing pattern,
+ * allowing the system to reconstruct the state by replaying events.
+ *
+ * @param {string} type - The type of the event for which the state rebuilder is registered.
+ * @param {string} context - The context in which the state rebuilder operates.
+ * @param {function(any): void} builderHandler - The state rebuilder function that applies
+ * the event to the state.
+ *
+ * @throws Will throw an error if a state rebuilder for the given event type and context
+ * already exists.
+ */
 
 export function registerStateRebuilder(
   type: string,
@@ -127,6 +194,14 @@ export function registerStateRebuilder(
   });
 }
 
+/**
+ * Applies an upcaster function to the given event if a matching upcaster is
+ * registered. If no upcaster is registered, the event is passed through unchanged.
+ *
+ * @param {CloudEvent<any>} event - The event to upcast or pass through.
+ * @returns {CloudEvent<any>} The upcasted event if an upcaster is registered,
+ * otherwise the original event.
+ */
 function maybeUpcast(event: CloudEvent<any>) {
   const upcaster = upcastHandler.get(event.type);
   if (upcaster) {
@@ -135,6 +210,14 @@ function maybeUpcast(event: CloudEvent<any>) {
   return event;
 }
 
+/**
+ * Executes a command and applies the returned events to the state.
+ *
+ * @param {Command<C>} command - The command to execute.
+ *
+ * @returns {Promise<void>} A promise that resolves when the command has been
+ * executed and the state has been updated.
+ */
 export async function handleCommand<C>(command: Command<C>) {
   const commandHandler = commands.get(command.type);
   if (commandHandler) {
@@ -162,6 +245,21 @@ export async function handleCommand<C>(command: Command<C>) {
     }
   }
 }
+/**
+ * Reconstructs the state for a given context and subjects by loading and processing historical events.
+ *
+ * @template C - The type of the state to be reconstructed.
+ *
+ * @param {string} context - The unique context for which the state is reconstructed.
+ * @param {string[]} subjects - An array of subjects used to load relevant events.
+ *
+ * @returns {Promise<any>} A promise that resolves to the reconstructed state.
+ *
+ * The function retrieves the state loading function associated with the provided context,
+ * loads historical events for the specified subjects, and applies each event to rebuild
+ * the current state. If no state loading function is found, an empty state is returned.
+ */
+
 export async function createState<C>(context: string, subjects: string[]) {
   const stateLoadingFunction = stateLoader.get(context);
   let events = stateLoadingFunction
@@ -176,6 +274,18 @@ export async function createState<C>(context: string, subjects: string[]) {
   return state;
 }
 
+/**
+ * Applies an event to the state by executing the associated state rebuilder.
+ *
+ * @param {string} context - The context of the event.
+ * @param {CloudEvent<any>} event - The event to be applied to the state.
+ * @param {any} [state=null] - The current state.
+ *
+ * @returns {any} The updated state.
+ *
+ * If no state rebuilder is registered for the given event type and context, the
+ * event is ignored and the state is returned unchanged.
+ */
 function executeEvent(context: string, event: CloudEvent<any>, state: null) {
   const rebuilder = stateRebuilder.get(createKey(context, event.type));
   if (rebuilder && rebuilder.context === context) {
@@ -184,14 +294,14 @@ function executeEvent(context: string, event: CloudEvent<any>, state: null) {
   return state;
 }
 
+/**
+ * Creates a unique key for the given context and event type.
+ *
+ * @param {string} context - The context of the event.
+ * @param {string} type - The type of the event.
+ *
+ * @returns {string} The unique key.
+ */
 function createKey(context: string, type: string): string {
   return `[${context}|${type}]`;
-}
-
-export function CommandHandler(
-  target: any,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) {
-  console.log(target, propertyKey, descriptor);
 }
